07404c7d2d9fee2f14061917c45b71f56c42329d
Gigabein <gigabein@gmail.com>
2014-08-11 12:33:15 -0600

    Update server.R

--- heros/server.R
+++ heros/server.R
@@ -1,8 +1,15 @@
 shinyServer(function(input, output) {
   getExpectedValue <- function(low, med, high) {
-      expectedValue = (6 - low)/22 * 200 + 
-                      (13 - med)/22 * 400 + 
-                      (3 - high)/22 * 600
+      totalCharacters = 37
+      maxNumLow = 7
+      maxNumMed = 25
+      maxNumHigh = 5
+      priceLow = 200
+      priceMed = 400
+      priceHigh = 600
+      expectedValue = (maxNumLow - low)/totalCharacters * priceLow + 
+                      (maxNumMed - med)/totalCharacters * priceMed + 
+                      (maxNumHigh - high)/totalCharacters * priceHigh
       return(expectedValue)
   }
   output$eV <- renderText({
